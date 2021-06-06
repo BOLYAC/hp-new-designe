1e465a9b177e1b8e8d9c2f0cbff1f8d4fdec1365
Bilel <bilell19@gmail.com>
2021-06-06 22:45:04 +0300

    Missing files

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Models\Property;
+use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
+
+class PropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $project = Project::findOrFail($request->project_id);
+        $project->properties()->create([
+            'unit_type' => $request->unit_type,
+            'flat_type' => $request->flat_type,
+            'floor' => $request->floor,
+            'gross_sqm' => $request->gross_sqm,
+            'net_sqm' => $request->net_sqm
+        ]);
+
+        return \view('projects.edit', compact('project'))
+            ->with('toast_danger', __("Successfully created new apartment"));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Property $property
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Property $property)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Property $property
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Property $property)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Property $property
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Property $property)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Property $property
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Property $property)
+    {
+        //
+    }
+}
